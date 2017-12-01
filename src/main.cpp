3d016eb5fb6187095e5309b8765c0eb4ddf497fe
https://github.com/orzsy <orzsy@qq.com>
2017-12-01 13:50:45 +0800

    add packed exec

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "QTextCodec"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon("myApp.ico"));
+//    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec *codec = QTextCodec::codecForName("GB2312");
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForTr(codec);
 
     MainWindow w;
     w.setWindowTitle("LRC时间戳手动抓取工具");
